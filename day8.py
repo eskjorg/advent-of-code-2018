8cbace76510cd4efdc8f2f6fd8c53a8ce916e332
Eskil Jörgensen <eskil.jorgensen@zenuity.com>
2018-12-09 17:36:47 +0100

    Update day 8

--- day8.py
+++ day8.py
@@ -6,40 +6,41 @@ with open('data/input8.txt', 'r') as f:
     DATA_INT = list(map(int, DATA_STR))
 
 class Node:
-	def __init__(self, parent=None, metadata=None):
+	def __init__(self, n_children, metadata=None, parent=None):
+		self.n_children = n_children
+		self.children = []
+		self.metadata = metadata
 		if parent:
 			self.parent = parent
-			parent.childs.append(self)
-		self.childs = []
-		self.metadata = metadata or []
+			parent.children.append(self)
+
+	def is_complete(self):
+		return self.n_children == len(self.children)
 
 	def sum_recursive(self):
-		sum_childs = [child.sum_recursive() for child in self.childs]
-		return sum(self.metadata) + sum(sum_childs) 
+		sum_children = [child.sum_recursive() for child in self.children]
+		return sum(self.metadata) + sum(sum_children) 
 
 	def value(self):
-		if len(self.childs) == 0:
+		if len(self.children) == 0:
 			return sum(self.metadata)
 		value = 0
 		for metadata in self.metadata:
-			if metadata <= len(self.childs):
-				value += self.childs[metadata - 1].value()
+			if metadata <= len(self.children):
+				value += self.children[metadata - 1].value()
 		return value
 
-root = current = Node(metadata=[0])
-node_stack = [[1, 0]]  # [c, m] = [n_childs, n_metadata]
+root = current = Node(1, metadata=[0])
 
 idx = 0
 while idx < len(DATA_INT):
-	if node_stack[-1][0] == 0:  # No more children -> collect metadata
-		_, n_metadata = node_stack.pop()
-		current.metadata.extend(DATA_INT[idx: idx + n_metadata])
+	if current.is_complete():
+		n_metadata = current.metadata
+		current.metadata = DATA_INT[idx: idx + n_metadata]
 		current = current.parent
 		idx += n_metadata
 	else:
-		node_stack[-1][0] -= 1
-		node_stack.append(DATA_INT[idx: idx + 2])
-		current = Node(parent=current)
+		current = Node(*DATA_INT[idx: idx + 2], parent=current)
 		idx += 2
 
 print("Part 1: ", root.sum_recursive())
