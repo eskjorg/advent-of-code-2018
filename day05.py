621f318d0e9cd31d9b4401966ce4a499dc6626b7
Eskil Jörgensen <eskil.jorgensen@zenuity.com>
2018-12-10 14:30:39 +0100

    Add day 5

---
+++ day05.py
@@ -0,0 +1,19 @@
+from string import ascii_lowercase as alphabet
+import numpy as np
+
+with open('data/input05.txt', 'r') as f:
+    DATA_STR = f.readline().rstrip()
+
+def react(polymer, unit=''):
+    polymer = polymer.replace(unit, '').replace(unit.upper(), '')
+    i = 0
+    while i < len(polymer) - 1:
+        if polymer[i] == polymer[i+1].swapcase():
+            polymer = polymer[:i] + polymer[i+2:]
+            i = max(0, i - 1)
+        else:
+            i += 1
+    return(len(polymer))
+
+print("Part 1: ", react(DATA_STR))
+print("Part 2: ", min([react(DATA_STR, alpha) for alpha in alphabet]))
