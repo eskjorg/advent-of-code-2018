9720c724ea6d50f732d9b846dfdd6c474db131b9
Eskil Jörgensen <eskil.jorgensen@gmail.com>
2019-06-11 13:46:39 +0200

    Add template

---
+++ template.py
@@ -0,0 +1,28 @@
+import re
+import numpy as np
+from collections import deque
+from itertools import product, cycle, repeat, count, permutations, combinations
+from functools import reduce
+
+with open('inputX.txt', 'r') as f:
+    lines = f.readlines()
+
+    data_str = [line.rstrip().split(" ") for line in lines]
+    #data_str = [line.rstrip().split("\t") for line in lines]
+
+    #data_str = [re.split('# |@ |, |: |x',line.rstrip()) for line in lines]
+
+    #newstr = ''.join((ch if ch in '0123456789.-e' else ' ') for ch in s)
+	#data_numbers = [float(i) for i in newstr.split()]
+
+    #data_int = [list(map(int, line.rstrip().split(","))) for line in lines]
+    #data_np = np.array(data_int)
+
+for line in data_XXX:
+	xxx
+
+
+
+print("Part 1: ", '')
+
+print("Part 2: ", '')
